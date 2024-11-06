e45aa04ffa38d8288cdef8166aab601f6253060f
CorentinGS <contact@corentings.dev>
2024-11-06 10:13:08 +0100

    feat(int): ✨ add SafeParse function for safe string to numeric conversion refactor: remove deprecated string parsing implementation

--- int.go
+++ int.go
@@ -1,5 +1,10 @@
 package safeconversion
 
+import (
+	"errors"
+	"strconv"
+)
+
 type Integer interface {
 	~int | ~uint | ~int8 | ~uint8 | ~int16 | ~uint16 | ~int32 | ~uint32 | ~int64 | ~uint64
 }
@@ -17,4 +22,32 @@ func CastInt[From Integer, To Integer](value From) (To, error) {
 	}
 
 	return converted, nil
+}
+
+var ErrOutOfRange = errors.New("value out of range for target type")
+
+type numericType interface {
+	~int | ~int32 | ~int64 | ~uint32 | ~uint64
+}
+
+func SafeParse[T numericType](value string) (T, error) {
+	var bitSize int
+	switch any(T(0)).(type) {
+	case int8, uint8:
+		bitSize = 8
+	case int16, uint16:
+		bitSize = 16
+	case int32, uint32:
+		bitSize = 32
+	case int64, uint64:
+		bitSize = 64
+	default:
+		bitSize = 64
+	}
+	numericValue, err := strconv.ParseInt(value, 10, bitSize)
+	if err != nil {
+		return T(0), err
+	}
+
+	return T(numericValue), nil
 }
